5fb546a4ec8be3c3513ed58fa9794985e00ddc64
Flavio Egoavil <fegoavil010@soa.ad.pwcinternal.com>
2018-11-21 16:21:12 -0500

    mejoras y ahora si se supone que funciona

--- legochain.lisp
+++ legochain.lisp
@@ -6,6 +6,7 @@
 (defclass blockchain ()
   ((blocks :initarg :blocks
            :initform nil
+           :reader blockchain-blocks
            :documentation "The blockchain itself, a list of blocks."))
   (:documentation "A blockchain."))
 
@@ -28,16 +29,20 @@
 
 (defclass block ()
   ((id :initarg :id
+       :reader block-id
        :documentation "Id of the current block.")
    (previous-hash :initarg :previous-hash
+                  :reader block-previous-hash
                   :type string)
    ;; (this-hash :initarg this-hash
    ;;            :documentation "Hash value of the current block.")
-   (timestamp :initarg :timestamp)
+   (timestamp :initarg :timestamp
+              :reader block-timestamp)
    (nonce-value :initarg :nonce-value
                 :documentation "Nonce value for this block.")
    (payload :initarg :payload
             :initform *dummy-value*
+            :reader block-payload
             :documentation "The block's usable (payload) data."
             :type payload-type))
   (:documentation "A block in the blockchain"))
@@ -103,14 +108,14 @@ That is, a block that satisfies the difficulty/challenge."
 
 
 
-(defmethod add-data-to-blockhain ((chain blockchain)
+(defmethod add-data-to-blockchain ((chain blockchain)
                                   payload)
   "Add data to the blockchain. 
 this automatically mines a new block with the payload."
   (declare (type payload-type payload))
   ;; get the id of the last object, the hash, etc
   (let* ((recent-block (last-block-on-blockchain chain))
-         (last-id (slot-value recent-block 'id))
+         (last-id (block-id recent-block))
          (last-hash (compute-hash recent-block)))
     ;; mine new block !
     (let ((new
@@ -122,46 +127,88 @@ this automatically mines a new block with the payload."
       (push-block-to-blockchain new chain))))
 
 
-(defparameter *my-blockchain*
-  (make-instance 'blockchain))
+(defvar *my-blockchain* nil)
+
 
 (defun startup ()
-  "Some startup tasks."
+  "Some startup tasks. This erases the in-memory blockchain!"
   ;; compile REGEX
   (unless *regex-difficulty*
     (setf *regex-difficulty*
           (cl-ppcre:create-scanner
            (regex-for-difficulty *mining-difficulty*))))
+
+  ;; new blockchain
+  (setf *my-blockchain* (make-instance 'blockchain))
   ;; my blockchain needs a new block... the first block
   (push-block-to-blockchain *my-blockchain*
-                            (make-instance 'block
-                                           :nonce-value 0
-                                           :timestamp (get-universal-time)
-                                           :previous-hash nil
-                                           :id 0)))
+                            (mine-new-block  
+                             :previous-hash nil
+                             :payload *dummy-value*
+                             :id 0)))
+
 
 (defmethod verify-blockchain ((chain blockchain))
-  "Check that all blocks comply the rules of the blockchain..."
-  (loop
-    :with previous = nil
-    :with good = T
-    :for b in (slot-value chain 'blocks) ; for every block (starting with latest)
-    :do
-    ;; verification
-    (setf good
-          (and good
-               (cond
-                 ((typep previous 'boolean) T)
-                 ((typep previous 'block)
-                  (string= (slot-value b 'previous-hash)
-                           (compute-hash previous)))
-                 )))
-    (setf previous b)
-    :return
-    good))
+  "Check that all blocks comply the rules of the blockchain..
+Returns array where all should be T"
+  (let ((blocks (blockchain-blocks chain)))
+     (maplist ;; applies successive CDR to the blocks list
+      (lambda (blocklist)
+        ;; The rules...
+        ;; verification of the list of blocks
+        (let  ((head (first blocklist))
+               (previous (second blocklist)))
+          ;; function that says head is good with previous
+          (or (null previous)         ; no previous block
+              ;; if there is a previous block:
+              (and 
+               ;; hash of previous block equals previous-hash of current block
+               (string= (block-previous-hash head)
+                        (compute-hash previous))
+               ;; timestamps are consecutive
+               ;; note: >= because (get-universal-time) only is precise to seconds.
+               (>= (block-timestamp head)
+                   (block-timestamp previous))
+               ;; ids are consecutive
+               (> (block-id head)
+                  (block-id previous))
+               ;; hash complies with challenge
+               (hash-complies-with-difficulty (compute-hash head))
+               ;; T here only so the AND block returns either T or NIL
+               T
+               ))))
+      blocks)))
+
+;;show all blockchain
+(defun show-blockchain ()
+  (mapcar (lambda (b)
+            (mapcar
+             (lambda (slot-name)
+               (list slot-name
+                     (slot-value b slot-name)))
+             (mapcar #'sb-mop:slot-definition-name
+                     (sb-mop:class-slots (class-of b)))))
+          (blockchain-blocks *my-blockchain*)))
 
 
-  
+
+(defun add-stuff ()
+  "Add test blocks to blockchain."
+  (loop for str in '("Interesting stuff"
+                     "Another block"
+                     "Yet another block"
+                     "WOW, blockchains are cool")
+        do
+        (add-data-to-blockchain
+         *my-blockchain*
+         (conspack:encode str))))
+
+(defun list-payloads ()
+  "Show all payloads (decoded)"
+  (loop for bl in (blockchain-blocks *my-blockchain*)
+        collecting (conspack:decode
+                    (block-payload bl))))
+
 
 
 
