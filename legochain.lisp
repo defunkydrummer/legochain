5c44200bcd681d7b01df41d3231100d978dbe7bb
Flavio Egoavil <fegoavil010@soa.ad.pwcinternal.com>
2018-11-21 15:08:54 -0500

    avances, se puede agregar bloque. Sin embargo, no funciona el metodo de verificacion.

--- legochain.lisp
+++ legochain.lisp
@@ -9,16 +9,28 @@
            :documentation "The blockchain itself, a list of blocks."))
   (:documentation "A blockchain."))
 
+(defmethod push-block-to-blockchain ((the-block block) (chain blockchain))
+  (with-slots (blocks) chain
+    ;; just push the block!
+    (push the-block blocks)))
+
+;; the last (recent) block on the chain
+(defmethod last-block-on-blockchain ((chain blockchain))
+  (with-slots (blocks) chain
+    (first blocks))) ;latest
+
 (defparameter *dummy-value*
   (conspack:encode "This block has a dummy value. Congratulations!"))
 
-(defparameter *mining-difficulty* 4
-  "Number of zeroes the hash must start with to satisfy the difficulty condition when mining.")
+;; data type of our payloads...
+(deftype payload-type () `(or null
+                              (simple-array (unsigned-byte 8))))
 
 (defclass block ()
   ((id :initarg :id
        :documentation "Id of the current block.")
-   (previous-hash :initarg :previous-hash)
+   (previous-hash :initarg :previous-hash
+                  :type string)
    ;; (this-hash :initarg this-hash
    ;;            :documentation "Hash value of the current block.")
    (timestamp :initarg :timestamp)
@@ -27,7 +39,7 @@
    (payload :initarg :payload
             :initform *dummy-value*
             :documentation "The block's usable (payload) data."
-            :type (or null (simple-array (unsigned-byte 8)))))
+            :type payload-type))
   (:documentation "A block in the blockchain"))
 
 ;; tell conspack how to encode such a block into a vector of bytes
@@ -46,6 +58,9 @@
     (conspack:decode payload)))
 
 
+(defparameter *mining-difficulty* 4
+  "Number of zeroes the hash must start with to satisfy the difficulty condition when mining.")
+
 (defvar *regex-difficulty* nil
   "Compiled REGEX for checking difficulty.")
 
@@ -58,15 +73,18 @@
   "True if the hash complies with difficulty spec."
   (declare (type string h))
   ;; if it complies with the regex (regex returns non-nil)
-  (cl-ppcre:scan-to-strings *regex-difficulty* h))
+  (if *regex-difficulty*
+      (cl-ppcre:scan-to-strings *regex-difficulty* h)
+      (error "REGEX for difficulty not compiled yet.")))
 
 (defun mine-new-block (&key previous-hash payload id)
-  "Mine a new block for the data "
+  "Mine (and return) a new block for the data.
+That is, a block that satisfies the difficulty/challenge."
   ;; a terrible loop that will keep the computer busy...
-  (let ((b nil) ;the final block
+  (let ((b nil)                         ;the final block
         (nonce-value 0))                            
     (loop
-      do
+      :do
       (setf b
             (make-instance 'block
                            :payload payload
@@ -77,7 +95,7 @@
                            ))
       (incf nonce-value)
       ;; compute hash until it complies with target challenge.
-      until (hash-complies-with-difficulty
+      :until (hash-complies-with-difficulty
              (compute-hash b)))
     ;;and we return the block thus mined
     b
@@ -85,12 +103,71 @@
 
 
 
+(defmethod add-data-to-blockhain ((chain blockchain)
+                                  payload)
+  "Add data to the blockchain. 
+this automatically mines a new block with the payload."
+  (declare (type payload-type payload))
+  ;; get the id of the last object, the hash, etc
+  (let* ((recent-block (last-block-on-blockchain chain))
+         (last-id (slot-value recent-block 'id))
+         (last-hash (compute-hash recent-block)))
+    ;; mine new block !
+    (let ((new
+            (mine-new-block
+             :previous-hash last-hash
+             :payload payload
+             :id (1+ last-id)))) ;with an ID that is superior to the last one.
+      ;; add this new block to the blockchain
+      (push-block-to-blockchain new chain))))
+
+
+(defparameter *my-blockchain*
+  (make-instance 'blockchain))
+
 (defun startup ()
   "Some startup tasks."
   ;; compile REGEX
   (unless *regex-difficulty*
     (setf *regex-difficulty*
           (cl-ppcre:create-scanner
-           (regex-for-difficulty *mining-difficulty*)))))
+           (regex-for-difficulty *mining-difficulty*))))
+  ;; my blockchain needs a new block... the first block
+  (push-block-to-blockchain *my-blockchain*
+                            (make-instance 'block
+                                           :nonce-value 0
+                                           :timestamp (get-universal-time)
+                                           :previous-hash nil
+                                           :id 0)))
+
+(defmethod verify-blockchain ((chain blockchain))
+  "Check that all blocks comply the rules of the blockchain..."
+  (loop
+    :with previous = nil
+    :with good = T
+    :for b in (slot-value chain 'blocks) ; for every block (starting with latest)
+    :do
+    ;; verification
+    (setf good
+          (and good
+               (cond
+                 ((typep previous 'boolean) T)
+                 ((typep previous 'block)
+                  (string= (slot-value b 'previous-hash)
+                           (compute-hash previous)))
+                 )))
+    (setf previous b)
+    :return
+    good))
+
+
+  
+
+
+
+
+
+
+
 
 
